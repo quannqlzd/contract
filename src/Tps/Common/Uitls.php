3e4690106561d3c17e31fe841d050ff44d791b58
Quan Nguyen Quoc <quan.nguyen.quoc@lazada.vn>
2016-09-08 15:40:46 +0700

    add Uitls.php

---
+++ src/Tps/Common/Uitls.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author Quan Nguyen Quoc <quan.nguyen.quoc@lazada.vn>
+ */
+
+namespace Tps\Common;
+
+class Uitls
+{
+    public static function sum ($a, $b) {
+        return $a + $b;
+    }
+
+}
